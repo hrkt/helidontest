c32f02725d875317c90c675777b51befc1ab1881
Hiroki Ito <hiroki110@gmail.com>
2019-02-21 07:33:26 +0900

    add ServerConfiguration

--- src/main/java/io/hrkt/helidontest/HelidontestApplication.java
+++ src/main/java/io/hrkt/helidontest/HelidontestApplication.java
@@ -5,14 +5,20 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
 import io.helidon.webserver.WebServer;
 
 public class HelidontestApplication {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        
+        int port = 8080;
+        
+        ServerConfiguration configuration = ServerConfiguration.builder().port(port).build();
+        Routing routing = Routing.builder()
+                .any((req, res) -> res.send("It works!")).build();
         WebServer webServer = WebServer
-                .create(Routing.builder()
-                        .any((req, res) -> res.send("It works!")).build())
+                .create(configuration, routing)
                 .start().toCompletableFuture().get(10, TimeUnit.SECONDS);
 
         System.out.println(
