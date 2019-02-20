ffd2e40d7fe6e8b4c8e837748e1e2c348bd5f383
Hiroki Ito <hiroki110@gmail.com>
2019-02-21 06:25:22 +0900

    add main class

--- src/main/java/io/hrkt/helidontest/HelidontestApplication.java
+++ src/main/java/io/hrkt/helidontest/HelidontestApplication.java
@@ -1,13 +1,22 @@
 package io.hrkt.helidontest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-@SpringBootApplication
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+
 public class HelidontestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelidontestApplication.class, args);
-	}
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        WebServer webServer = WebServer
+                .create(Routing.builder()
+                        .any((req, res) -> res.send("It works!")).build())
+                .start().toCompletableFuture().get(10, TimeUnit.SECONDS);
+
+        System.out.println(
+                "Server started at: http://localhost:" + webServer.port());
+    }
 
 }
