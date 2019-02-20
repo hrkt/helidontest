c91188dff4fee2fec8ffd239ac19c489fb88de18
Hiroki Ito <hiroki110@gmail.com>
2019-02-21 05:56:19 +0900

    initial commit

---
+++ src/main/java/io/hrkt/helidontest/HelidontestApplication.java
@@ -0,0 +1,13 @@
+package io.hrkt.helidontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelidontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelidontestApplication.class, args);
+	}
+
+}
